6f3fbaa7999824161a712c224f32fb55f0af42b8
Stéphane Klein <contact@stephane-klein.info>
2022-07-23 09:26:14 +0200

    Add vim-svelte

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -13,4 +13,13 @@ return require('packer').startup(function()
 
   -- Git
   use 'tpope/vim-fugitive'
+
+  -- Svelte packages (https://github.com/evanleck/vim-svelte)
+  use {
+    'evanleck/vim-svelte',
+    requires = {
+      {'othree/html5.vim'},
+      {'pangloss/vim-javascript'}
+    }
+  }
 end)
