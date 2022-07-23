38af498a8865efa07be4c6cc34cef6282b5fe58e
Stéphane Klein <contact@stephane-klein.info>
2022-07-23 23:05:48 +0200

    Change 2 spaces indent to 4 spaces

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,25 +1,25 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  -- Theme
-  use { 'morhetz/gruvbox' }
-
-  -- Git
-  use 'tpope/vim-fugitive'
-
-  -- Svelte packages (https://github.com/evanleck/vim-svelte)
-  use {
-    'evanleck/vim-svelte',
-    requires = {
-      {'othree/html5.vim'},
-      {'pangloss/vim-javascript'}
+    use 'wbthomason/packer.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
     }
-  }
+
+    -- Theme
+    use { 'morhetz/gruvbox' }
+
+    -- Git
+    use 'tpope/vim-fugitive'
+
+    -- Svelte packages (https://github.com/evanleck/vim-svelte)
+    use {
+        'evanleck/vim-svelte',
+        requires = {
+            {'othree/html5.vim'},
+            {'pangloss/vim-javascript'}
+        }
+    }
 end)
