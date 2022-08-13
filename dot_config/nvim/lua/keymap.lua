64ddb7e5735fd0ffd43795ebd8fa7b5b0dadc0ab
Stéphane Klein <contact@stephane-klein.info>
2022-08-13 22:53:04 +0200

    Add new LSP keymaps

--- dot_config/nvim/lua/keymap.lua
+++ dot_config/nvim/lua/keymap.lua
@@ -78,3 +78,15 @@ vim.keymap.set('n', '<leader>t<down>', ":lua open_bottom_terminal()<CR>")
 -- Emmet
 vim.g['user_emmet_leader_key'] = '<C-e>'
 
+-- LSP
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = '[R]e[n]ame' })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = '[C]ode [A]ction' })
+
+vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { desc = '[G]oto [D]efinition' })
+vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { desc = '[G]oto [I]mplementation' })
+vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references)
+vim.keymap.set('n', '<leader>ds', require('telescope.builtin').lsp_document_symbols, { desc = '[D]ocument [S]ymbols' })
+vim.keymap.set('n', '<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = '[W]orkspace [S]ymbols' })
+
+vim.keymap.set('n', 'h', vim.lsp.buf.hover, { desc = 'Hover Documentation' })
+vim.keymap.set('n', '<C-h>', vim.lsp.buf.signature_help, { desc = 'Signature Documentation' })
