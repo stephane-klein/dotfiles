3513de913055285c4ed1a836c3626006c30f67c7
Stéphane Klein <contact@stephane-klein.info>
2022-08-14 19:39:57 +0200

    Disable css LSP server

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -1,8 +1,10 @@
    -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-require('lspconfig')['cssls'].setup({
-    capabilities = capabilities
-})
+
+-- Disable because this issue: https://github.com/stephane-klein/dotfiles/issues/27
+-- require('lspconfig')['cssls'].setup({
+--     capabilities = capabilities
+-- })
 require('lspconfig')['svelte'].setup({
     capabilities = capabilities
 })
