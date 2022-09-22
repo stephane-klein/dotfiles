44d431e89ff26429d29fb1e32a6925cc2d0fc471
Stéphane Klein <contact@stephane-klein.info>
2022-09-22 23:16:48 +0200

    Add Neovim Neo-tree git keymap #58

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -199,7 +199,14 @@ return require('packer').startup(function()
                         ["<space>"] = {
                             "toggle_node",
                             nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
-                        }
+                        },
+                        ["A"]  = "git_add_all",
+                        ["gu"] = "git_unstage_file",
+                        ["ga"] = "git_add_file",
+                        ["gr"] = "git_revert_file",
+                        ["gc"] = "git_commit",
+                        ["gp"] = "git_push",
+                        ["gg"] = "git_commit_and_push"
                     }
                 },
                 filesystem = {
