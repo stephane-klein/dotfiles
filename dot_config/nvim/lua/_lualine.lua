251036194302ec8ecda0080048e46d7aad384aa2
Stéphane Klein <contact@stephane-klein.info>
2022-10-03 10:14:47 +0200

    Add filename in Neovim winbar #62

--- dot_config/nvim/lua/_lualine.lua
+++ dot_config/nvim/lua/_lualine.lua
@@ -1,15 +1,36 @@
 require('lualine').setup({
     options = {
         theme = 'tokyonight',
+        globalstatus = true
     },
     extensions = { 'fugitive' },
     sections = {
-        lualine_a = {
+        lualine_c = {}
+    },
+    winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
             {
                 'filename',
-                file_status = true,
                 path = 1
             }
-        }
+        },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+    },
+    inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+            {
+                'filename',
+                path = 1
+            }
+        },
+        lualine_x = {},
+      lualine_y = {},
+      lualine_z = {}
     }
 })
