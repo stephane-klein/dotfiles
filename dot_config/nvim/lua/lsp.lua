7602b1f0c4ad308f83630a5b855ad27e1d2817e9
Stéphane Klein <contact@stephane-klein.info>
2022-11-11 22:16:08 +0100

    Fix bug

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -1,6 +1,6 @@
 -- Setup lspconfig.
 local nvim_lsp = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- Disable because this issue: https://github.com/stephane-klein/dotfiles/issues/27
 -- nvim_lsp['cssls'].setup({
