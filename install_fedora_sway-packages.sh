444c5a66b085dcc36577aa9c41bd3b2eeb7e124d
Stéphane Klein <contact@stephane-klein.info>
2023-02-10 13:29:05 +0100

    WIP

--- install_fedora_sway-packages.sh
+++ install_fedora_sway-packages.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 sudo dnf install sway sway-systemd waybar fuzzel swaybg swayidle swaylock wshowkeys mako brightnessctl \
-    inotify-tools # ~/.config/waybar/launch-waybar.sh script dependency
+    inotify-tools \ # ~/.config/waybar/launch-waybar.sh script dependency
+    wtype # for https://github.com/marty-oehme/bemoji
 
