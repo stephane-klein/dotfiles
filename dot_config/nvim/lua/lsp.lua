6e588e3443bd709dddbf247bb4dac01bf3c97454
Stéphane Klein <contact@stephane-klein.info>
2023-12-26 12:03:49 +0100

    Disable a11y redendant roles warning in Neovim in Svelte project #129

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -6,8 +6,21 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protoc
 -- nvim_lsp['cssls'].setup({
 --     capabilities = capabilities
 -- })
-nvim_lsp['svelte'].setup({
-    capabilities = capabilities
+nvim_lsp.svelte.setup({
+    capabilities = capabilities,
+    settings = {
+        svelte = {
+            plugin = {
+                svelte = {
+                    compilerWarnings = {
+                        -- I found this configuration here:
+                        -- https://github.com/DaikyXendo/dotfile/blob/7ff5f84b170d6a50365a775c79bbd5dd84cd6093/nvim/lua/plug_configs/lspconfig.lua#L159
+                        ["a11y-no-redundant-roles"] = "ignore"
+                    }
+                }
+            }
+        }
+    }
 })
 nvim_lsp['html'].setup{
     capabilities = capabilities
