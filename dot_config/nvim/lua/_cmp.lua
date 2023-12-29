ee13f111b8a98f7a24046162d54137263533906c
Stéphane Klein <contact@stephane-klein.info>
2023-12-29 22:46:22 +0100

    Configure luasnip jump keymap

--- dot_config/nvim/lua/_cmp.lua
+++ dot_config/nvim/lua/_cmp.lua
@@ -1,5 +1,6 @@
 vim.g.completeopt='menu,menuone,noselect'
 local cmp = require('cmp')
+local luasnip = require('luasnip')
 cmp.setup({
     completion = {
         autocomplete = false
@@ -23,6 +24,34 @@ cmp.setup({
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<Tab>"] = cmp.mapping(
+            function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                    -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                    -- that way you will only jump inside the snippet region
+                elseif luasnip.expand_or_jumpable() then
+                   luasnip.expand_or_jump()
+                elseif has_words_before() then
+                   cmp.complete()
+                else
+                    fallback()
+                end
+            end,
+            { "i", "s" }
+        ),
+        ["<S-Tab>"] = cmp.mapping(
+            function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end,
+            { "i", "s" }
+        ),
     }),
 })
 cmp.setup.cmdline('/', {
