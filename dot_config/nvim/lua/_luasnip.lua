ede52e13da89ac51177026cf90026970bc831479
stephane-klein <contact@stephane-klein.info>
2024-04-09 07:45:13 +0200

    Add .tool-versions snippet

--- dot_config/nvim/lua/_luasnip.lua
+++ dot_config/nvim/lua/_luasnip.lua
@@ -2,6 +2,7 @@ require('snippets/docker-compose')
 require('snippets/javascript')
 require('snippets/postgres')
 require('snippets/scripts')
+require('snippets/all')
 
 local luasnip = require('luasnip')
 luasnip.filetype_extend("svelte", {"javascript", "html"});
