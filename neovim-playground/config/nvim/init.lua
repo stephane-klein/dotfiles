a3e0b19ae074da0a4ce5001a1dee5f49abbfc854
stephane-klein <contact@stephane-klein.info>
2024-05-19 16:50:15 +0200

    Setup first version of LSP

--- neovim-playground/config/nvim/init.lua
+++ neovim-playground/config/nvim/init.lua
@@ -106,5 +106,23 @@ require("lazy").setup({
                 { desc = "[S]earch [S]elect Telescope"}
             )
         end
+    },
+    { -- LSP Configuration & Plugins
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            -- Automatically install LSPs and related tools to stdpath for Neovim
+            { "williamboman/mason.nvim", config = true }, -- NOTE: Must be loaded before dependants
+            "williamboman/mason-lspconfig.nvim",
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+
+            -- Useful status updates for LSP.
+            -- NOTE: `opts = {}` is the same as calling `require("fidget").setup({})`
+            { "j-hui/fidget.nvim", opts = {} },
+
+            -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+            -- used for completion, annotations and signatures of Neovim apis
+            { "folke/neodev.nvim", opts = {} },
+        }
+    },
     }
 })
