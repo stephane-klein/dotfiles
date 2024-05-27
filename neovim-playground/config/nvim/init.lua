b67e319a6de0d105b3bbe10d3f60fbf2a330faff
stephane-klein <contact@stephane-klein.info>
2024-05-27 23:32:34 +0200

    Add nvim-lualine/lualine.nvim script

--- neovim-playground/config/nvim/init.lua
+++ neovim-playground/config/nvim/init.lua
@@ -281,11 +281,53 @@ require("lazy").setup({
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function ()
             require"alpha".setup(require"alpha.themes.startify".config)
-            end
+        end
     },
     {"gpanders/editorconfig.nvim"},
     {
         "hiphish/rainbow-delimiters.nvim",
         event = "VeryLazy",
     },
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("lualine").setup({
+                    options = {
+                    theme = "catppuccin",
+                    globalstatus = true
+                    },
+                    extensions = { "fugitive" },
+                    sections = {
+                    lualine_c = {}
+                    },
+                    winbar = {
+                    lualine_a = {},
+                    lualine_b = {},
+                    lualine_c = {
+                    {
+                    "filename",
+                    path = 1
+                    }
+                    },
+                    lualine_x = {},
+                    lualine_y = {},
+                    lualine_z = {}
+                    },
+                    inactive_winbar = {
+                        lualine_a = {},
+                        lualine_b = {},
+                        lualine_c = {
+                            {
+                                "filename",
+                                path = 1
+                            }
+                        },
+                        lualine_x = {},
+                        lualine_y = {},
+                        lualine_z = {}
+                    }
+            })
+        end
+    }
 })
