0b64cdc2d2a15de03d754befebfb716d2263a3a8
stephane-klein <contact@stephane-klein.info>
2024-06-05 11:16:07 +0200

    Improve nvim-cmp keymap and configuration

--- neovim-playground/config/nvim/init.lua
+++ neovim-playground/config/nvim/init.lua
@@ -233,27 +233,22 @@ require("lazy").setup({
                         luasnip.lsp_expand(args.body)
                     end
                 },
-                completion = { completeopt = "menu,menuone,noinsert" },
+                completion = {
+                    autocomplete = false,
+                    completeopt = "menu,menuone,noinsert"
+                },
                 mapping = cmp.mapping.preset.insert {
-                    -- Select the [n]ext item
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    -- Select the [p]revious item
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
 
                     -- Scroll the documentation window [b]ack / [f]orward
                     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-                    -- Accept ([y]es) the completion.
-                    --  This will auto-import if your LSP supports it.
-                    --  This will expand snippets if the LSP sent a snippet.
-                    ["<C-Space>"] = cmp.mapping.confirm { select = true },
 
-                    -- If you prefer more traditional completion keymaps,
-                    -- you can uncomment the following lines
-                    --["<CR>"] = cmp.mapping.confirm { select = true },
-                    --["<Tab>"] = cmp.mapping.select_next_item(),
-                    --["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                    ["<CR>"] = cmp.mapping.confirm { select = true },
+                    -- Select the [n]ext item
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    -- Select the [p]revious item
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
 
                     -- Manually trigger a completion from nvim-cmp.
                     --  Generally you don"t need this, because nvim-cmp will display
