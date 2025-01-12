3eff8a89830e397b028e9f80992e2ca17c2bbce0
stephane-klein <contact@stephane-klein.info>
2025-01-12 20:01:27 +0100

    Add Neovim Avante AI Code assistant
    More information in french: https://notes.sklein.xyz/Projet%2021/

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -732,6 +732,54 @@ require("lazy").setup({
     {
         'numToStr/Comment.nvim',
         opts = {}
+    },
+    -- Avante (Code Assistant) https://github.com/yetone/avante.nvim/
+    {
+        "yetone/avante.nvim",
+        -- event = "VeryLazy",
+        lazy = false,
+        version = false,
+        opts = {
+            provider = "copilot",
+            auto_suggestions_provider = "copilot",
+            copilot = {
+                model = "claude-3.5-sonnet"
+            }
+        },
+        build = "make",
+        dependencies = {
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "hrsh7th/nvim-cmp",
+            "nvim-tree/nvim-web-devicons",
+            {
+                "zbirenbaum/copilot.lua",
+                config = function()
+                    require("copilot").setup({})
+                end
+            },
+            {
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                    },
+                },
+            },
+            {
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
+        }
     }
 })
 
