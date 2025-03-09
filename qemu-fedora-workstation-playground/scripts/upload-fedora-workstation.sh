f235c2dc5db5490e468009d0574d975e36752c02
stephane-klein <contact@stephane-klein.info>
2025-03-09 23:49:54 +0100

    Fix bug

--- qemu-fedora-workstation-playground/scripts/upload-fedora-workstation.sh
+++ qemu-fedora-workstation-playground/scripts/upload-fedora-workstation.sh
@@ -3,6 +3,7 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-virt-sysprep --operations cloud-init -d fedora-working-layer.qcow2
+guestfish --rw -a fedora-working-layer.qcow2 -i rm-rf /var/lib/cloud
+
 qemu-img convert -p -O qcow2 -c -S 0 fedora-working-layer.qcow2 fedora-workstation-sklein.qcow2
 rclone copy -P fedora-workstation-sklein.qcow2 scaleway:fedora-workstation
