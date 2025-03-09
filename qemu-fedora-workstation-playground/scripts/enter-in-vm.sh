51acf396978aeb4d97e4558b9e9d0dcb58f5be8b
stephane-klein <contact@stephane-klein.info>
2025-03-09 23:49:35 +0100

    Replace `fedora` Unix user by `stephane`

--- qemu-fedora-workstation-playground/scripts/enter-in-vm.sh
+++ qemu-fedora-workstation-playground/scripts/enter-in-vm.sh
@@ -4,4 +4,4 @@ set -e
 cd "$(dirname "$0")/../"
 
 ssh-keygen -R "[localhost]:2222"
-ssh -o StrictHostKeyChecking=no -p 2222 fedora@localhost
+ssh -o StrictHostKeyChecking=no -p 2222 stephane@localhost
--- qemu-fedora-workstation-playground/scripts/install-vm-workstation.sh
+++ qemu-fedora-workstation-playground/scripts/install-vm-workstation.sh
@@ -4,9 +4,9 @@ set -e
 cd "$(dirname "$0")/../"
 
 ssh-keygen -R "[localhost]:2222"
-ssh -o StrictHostKeyChecking=no -p 2222 fedora@localhost "bash -s" < _payload_install-vm-workstation.sh
+ssh -o StrictHostKeyChecking=no -p 2222 stephane@localhost "bash -s" < _payload_install-vm-workstation.sh
 
-while ! ssh -q -o ConnectTimeout=5 -o BatchMode=yes -o StrictHostKeyChecking=no -p 2222 fedora@localhost exit&>/dev/null; do
+while ! ssh -q -o ConnectTimeout=5 -o BatchMode=yes -o StrictHostKeyChecking=no -p 2222 stephane@localhost exit&>/dev/null; do
   echo "Wait SSH ready..."
   sleep 5
 done
