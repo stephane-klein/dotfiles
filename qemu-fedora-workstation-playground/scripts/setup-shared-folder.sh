abdf55b882128571cbdf6072d8690b5151058189
stephane-klein <contact@stephane-klein.info>
2025-03-09 23:50:05 +0100

    WIP

--- qemu-fedora-workstation-playground/scripts/setup-shared-folder.sh
+++ qemu-fedora-workstation-playground/scripts/setup-shared-folder.sh
@@ -4,9 +4,9 @@ set -e
 cd "$(dirname "$0")/../"
 
 ssh-keygen -R "[localhost]:2222"
-ssh -o StrictHostKeyChecking=no -p 2222 fedora@localhost "bash -s" << 'EOF'
-sudo mkdir -p /mnt/host_share
-if ! mountpoint -q /mnt/host_share; then
-    sudo mount -t 9p -o trans=virtio,version=9p2000.L host_share /mnt/host_share
+ssh -o StrictHostKeyChecking=no -p 2222 stephane@localhost "bash -s" << 'EOF'
+mkdir -p /home/stephane/.local/share
+if ! mountpoint -q /home/stephane/.local/share; then
+    sudo mount -t 9p -o trans=virtio,version=9p2000.L,uid=$(id -u),gid=$(id -g) host_share /home/stephane/.local/share
 fi
 EOF
