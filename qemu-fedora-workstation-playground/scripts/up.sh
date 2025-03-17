9c8037bf7aa9510b6f09a111715e82a3711293ef
stephane-klein <contact@stephane-klein.info>
2025-03-17 21:38:12 +0100

    WIP

--- qemu-fedora-workstation-playground/scripts/up.sh
+++ qemu-fedora-workstation-playground/scripts/up.sh
@@ -14,22 +14,31 @@ done
 
 if [ ! -f "fedora-41-base.qcow2" ]; then
     if [ $USE_FEDORA_CLOUD_UPSTREAM_IMAGE -eq 1 ]; then
-        wget -q \
+        wget -q --progress=bar:force \
             https://download.fedoraproject.org/pub/fedora/linux/releases/41/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-41-1.4.x86_64.qcow2 \
             -O fedora-41-base.qcow2
     else
-        wget -q \
+        wget --progress=bar:force \
             https://fedora-workstation.s3.fr-par.scw.cloud/fedora-workstation-sklein.qcow2 \
             -O fedora-41-base.qcow2
+
     fi
 fi
 
-cloud-localds cloud-init.img cloud-init.yaml
+ACTUAL_SIZE=$(virt-filesystems --long --parts --blkdevs -h -a "fedora-41-base.qcow2" | grep "/dev/sda " | awk '{print $4}')
+
+if [ "$ACTUAL_SIZE" != "10G" ]; then
+    qemu-img create -f qcow2 -o preallocation=metadata fedora-41-base-resized.qcow2 10G
+    virt-resize --expand /dev/sda4 fedora-41-base.qcow2 fedora-41-base-resized.qcow2
+    rm fedora-41-base.qcow2
+    mv fedora-41-base-resized.qcow2 fedora-41-base.qcow2
+fi
 
 if [ ! -f "fedora-working-layer.qcow2" ]; then
     qemu-img create -f qcow2 -b fedora-41-base.qcow2 -F qcow2 fedora-working-layer.qcow2
 fi
-qemu-img resize fedora-working-layer.qcow2 10G
+
+cloud-localds cloud-init.img cloud-init.yaml
 
 qemu-system-x86_64 \
     -m 8G \
