961aa1ef6289271307f2b64f4c5f1293b218b964
Stéphane Klein <contact@stephane-klein.info>
2025-12-28 09:55:36 +0100

    Add the shorter alias `lz` to launch lazyvim

--- dot_oh-my-zsh/custom/lazyvim.zsh
+++ dot_oh-my-zsh/custom/lazyvim.zsh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
 alias lazyvim="NVIM_APPNAME='lazyvim-playground' nvim"
+alias lv="NVIM_APPNAME='lazyvim-playground' nvim"
+alias avante="NVIM_APPNAME='lazyvim-playground' nvim -c 'lua vim.defer_fn(function() require(\"avante.api\").zen_mode() end, 100)'"
